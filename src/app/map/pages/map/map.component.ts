1dd570a1f4dd116d3a712914fdfb49476f9b3911
Toch <tochukwu.claver@gmail.com>
2018-08-18 00:05:40 -0400

    added geofire x

--- src/app/map/pages/map/map.component.ts
+++ src/app/map/pages/map/map.component.ts
@@ -1,5 +1,8 @@
+import { CurrentLocation } from './../../../models/user.model';
 import { Component, OnInit } from '@angular/core';
 import { GeolocationService } from '../../../services/geolocation.service';
+import { BehaviorSubject, Observable, combineLatest, pipe } from 'rxjs';
+import { switchMap, tap, throttleTime, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-map',
@@ -8,13 +11,42 @@ import { GeolocationService } from '../../../services/geolocation.service';
 })
 export class MapComponent implements OnInit {
 
+  public radius = new BehaviorSubject<number>(0.5);
+  public center = new BehaviorSubject<CurrentLocation>(null);
 
+  public places$: Observable<any[]>;
 
   constructor(
     public geo: GeolocationService
   ) { }
 
   ngOnInit() {
+    this.places$ = combineLatest(this.getRad(), this.getCenter()).pipe(
+      switchMap(([rad, center]) => this.geo.getLocations(rad, center)),
+      tap((placs) => console.log('the places', placs))
+    );
+  }
+
+  public updateRad(rad: number) {
+   this.radius.next(rad);
+  }
+
+  public updateCenter(center: CurrentLocation) {
+    this.center.next(center);
+  }
+
+
+  private getCenter() {
+    return this.center.pipe(
+      throttleTime(400)
+    );
+  }
+
+  private getRad() {
+    return this.radius.pipe(
+      debounceTime(1000),
+      // distinctUntilChanged()
+    );
   }
 
 }
--- src/app/services/geolocation.service.ts
+++ src/app/services/geolocation.service.ts
@@ -1,7 +1,11 @@
-import { switchMap } from 'rxjs/operators';
+import { switchMap, shareReplay, filter } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { CurrentLocation } from '../models/user.model';
 import { Observable, of, throwError } from 'rxjs';
+
+import * as firebase from 'firebase/app';
+import * as geofirex from 'geofirex';
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +13,30 @@ import { Observable, of, throwError } from 'rxjs';
 export class GeolocationService {
 
   public currentLocation$: Observable<CurrentLocation>;
+  public places$: Observable<any>;
+
+  private geo = geofirex.init(firebase);
+  private locations = this.geo.collection('places');
 
   constructor() {
     this.currentLocation$ = this.getCurrentLocation();
   }
+
+
+  public getLocations(radius?: number, cntr?: CurrentLocation) {
+    return this.currentLocation$.pipe(
+      filter((res) => !!res),
+      switchMap((loc) => {
+        const center = !!cntr ? this.geo.point(cntr.lat, cntr.lng) : this.geo.point(loc.lat, loc.lng);
+        const rad = radius || 0.5;
+        const field = 'pos';
+        return this.locations.within(center, rad, field);
+      })
+    );
+  }
+
+
+
 
   private getCurrentLocation() {
    return of('geolocation' in navigator)
@@ -21,7 +45,8 @@ export class GeolocationService {
             if (!val) { return throwError('Your browser does not support geolocation'); }
             return of(val);
         }),
-        switchMap(() => this.getCurrentPosition())
+        switchMap(() => this.getCurrentPosition()),
+        shareReplay(1)
     );
   }
 
