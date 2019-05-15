f8ac15761324a7bc33e32992efb0c163e0226ea4
Tochukwuibe <tochukwu.claver@gmail.com>
2019-05-14 22:29:07 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,7 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyDXzdskRbzm7eryia7CzQULvumms4kvzPA',
-    authDomain: 'chatmap-f9fb4.firebaseapp.com',
-    databaseURL: 'https://chatmap-f9fb4.firebaseio.com',
-    projectId: 'chatmap-f9fb4',
-    storageBucket: 'chatmap-f9fb4.appspot.com',
-    messagingSenderId: '313871994122'
+    
   },
-  googleMapsKey: {apiKey: 'AIzaSyDXzdskRbzm7eryia7CzQULvumms4kvzPA'}
+  googleMapsKey: {apiKey: ''}
 };
